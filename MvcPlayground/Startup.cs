f88b2e867a54eb9c49c7b93b97ed2a5449b0b950
artiquno <endri-kurushi@hotmail.com>
2017-09-24 12:20:32 +0200

    Add project files.

---
+++ MvcPlayground/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPlayground.Startup))]
+namespace MvcPlayground
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

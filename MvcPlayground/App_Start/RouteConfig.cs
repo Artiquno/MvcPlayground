ad46dacade6ad598c1fe105995212fe506f3845c
artiquno <endri-kurushi@hotmail.com>
2017-10-08 09:27:35 +0200

    Code cleanup

--- MvcPlayground/App_Start/RouteConfig.cs
+++ MvcPlayground/App_Start/RouteConfig.cs
@@ -18,11 +18,6 @@ namespace MvcPlayground
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-            routes.MapRoute(
-                name: "Api",
-                url: "api/{controller}",
-                defaults: new { controller = "registerapi" }
-            );
         }
     }
 }

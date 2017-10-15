1d65b8ee9f96664f8867d6c90f14c5d2d22169ec
artiquno <endri-kurushi@hotmail.com>
2017-10-15 20:31:04 +0200

    Song/Create uses angular

---
+++ MvcPlayground/_Helpers/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPlayground.Helpers
+{
+    public static class FileHelper
+    {
+        public const string ImagePath = "~/Uploads/Images";
+
+        public static string SaveFile(HttpPostedFileBase file, string path = "~/Uploads")
+        {
+            string res = HttpContext.Current.Server.MapPath(path + "/" + file.FileName);
+            //file.SaveAs(path + file.FileName)
+            return res;
+        }
+
+        public static string SaveImage(HttpPostedFileBase file)
+        {
+            return SaveFile(file, "~/Uploads/Images");
+        }
+    }
+}

644dc165400456249f647a0759947c6a2273eabb
artiquno <endri-kurushi@hotmail.com>
2017-11-05 19:35:55 +0100

    Moved chat boxes to a directive

---
+++ MvcPlayground/Scripts/angular/directives/chat.js
@@ -0,0 +1,20 @@
+﻿angular.module("chat", [])
+    .directive("chat", ["$http", function ($http) {
+        return {
+            templateUrl: "/Scripts/angular/templates/chat.html",
+            restrict: "E",
+            scope: {
+                chat: "=chatObject"
+            },
+            link: function ($scope, $element, $attrs) {
+                $scope.minimize = function () {
+                    $($element).find(".chat").slideToggle(300, 'swing');
+                }
+
+                $scope.close = function (e) {
+                    e.stopPropagation();
+                    $($element).find(".live-chat").fadeOut(300);
+                }
+            } 
+        }        
+    }]);
